7e36da9aed24a45657cd99b083f00f6e7e262cd6
daxue0929 <daxue0929@qq.com>
2021-06-12 12:43:11 +0800

    [remote-config] Spring Cloud远程配置

---
+++ remote-config/src/main/java/com/daxue/remoteconfig/RemoteConfigApplication.java
@@ -0,0 +1,18 @@
+package com.daxue.remoteconfig;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+@Slf4j
+public class RemoteConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RemoteConfigApplication.class, args);
+        log.info("服务启动成功");
+    }
+
+}
---
+++ remote-config/src/test/java/com/daxue/remoteconfig/RemoteConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daxue.remoteconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemoteConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
