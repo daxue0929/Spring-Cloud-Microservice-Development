c6e2caafc8e3895b3ebf3de29ded195c686f7600
daxue0929 <daxue0929@qq.com>
2021-06-14 18:47:06 +0800

    [remote-config][demo]远程配置中心,注入Eureka

--- demo/src/main/java/com/example/demo/controller/Test.java
+++ demo/src/main/java/com/example/demo/controller/Test.java
@@ -3,8 +3,10 @@ package com.example.demo.controller;
 import com.example.demo.service.TestRemoteClient;
 import com.example.demo.service.TestService;
 import com.netflix.discovery.EurekaClient;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2021/05/21
  **/
 @RestController
+@Slf4j
 public class Test {
 
 
@@ -54,6 +57,16 @@ public class Test {
     @PostMapping("/fegin2")
     public String postRequestTest(@RequestParam String id) {
         return remoteClient.postRequestTest(id);
+    }
+
+
+    @Value("${test.username}")
+    public String username;
+
+    @GetMapping("/getRemoteUsername")
+    public String remoteConfig() {
+        log.info("remote config username: {}", username);
+        return username;
     }
 
 }
--- remote-config/src/main/java/com/daxue/remoteconfig/RemoteConfigApplication.java
+++ remote-config/src/main/java/com/daxue/remoteconfig/RemoteConfigApplication.java
@@ -3,11 +3,13 @@ package com.daxue.remoteconfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
 @Slf4j
+@EnableDiscoveryClient
 public class RemoteConfigApplication {
 
     public static void main(String[] args) {
